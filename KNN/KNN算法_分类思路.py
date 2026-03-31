ea7da5a8cad410a990d6dbf1e91979dca64bdcd0
leimen <leimen176@gmail.com>
2026-03-31 14:44:29 +0800

    KNN-鸢尾花案例

--- KNN/KNN算法_分类思路.py
+++ KNN/KNN算法_分类思路.py
@@ -13,8 +13,8 @@
 KNN算法，分类问题思路如下：
       1. 计算测试集和每个训练集样本之间的距离
       2. 基于距离进行升序排序
-      3. 找到最近的K个样本
-      4. K个样本进行投票
+      3. 通过欧式距离/曼哈顿距离/切比雪夫距离等计算距离。找到最近的K个样本
+      4. ***K个样本进行投票***
       5. 票数多的结果，作为最终的预测结果
 代码实现思路：
       1. 导包
@@ -25,21 +25,23 @@ KNN算法，分类问题思路如下：
 '''
 
 from sklearn.neighbors import KNeighborsClassifier
-
+    #distance :5    4    3    2
 # x_train = [[0], [1], [2], [3]]
 # y_train = [0 , 0 , 1 , 1]
 # x_test = [[5]]
 
 x_train = [[0], [1], [2], [3]]
-# y_train = ["a" , "a" , "b" , "b"]         #['a']
+# y_train = ["a" , "b" , "b" , "a"]         #['a']
 # y_train = ["b" , "b" , "z" , "z"]         #['z']
 # y_train = ["b" , "b" , "a" , "a"]           #['a']
 # y_train = ["中" , "中" , "国" , "国"]       #['中']
-y_train = ["王" , "王" , "玉玉玉玉" , "玉玉玉玉"]       #['玉玉玉玉']
+# y_train = ["王" , "王" , "玉玉玉玉" , "玉玉玉玉"]       #['玉玉玉玉']
 # y_train = ["爱情" , "爱情" , "文艺" , "文艺"]   #['文艺']
+y_train = [100, 200, 300, 400]
+y_train = [100, 200, 300, 299]
 x_test = [[5]]
-
-estimator  = KNeighborsClassifier(n_neighbors=4)
-estimator.fit(x_train, y_train)
-y_pred = estimator.predict(x_test)
+#选择模型，使用knn算法中分类,当最近邻有个的时候，会根据奥卡姆剃刀原则选择最简单的
+estimator  = KNeighborsClassifier(n_neighbors=2)
+estimator.fit(x_train, y_train)  #训练数据
+y_pred = estimator.predict(x_test) #数据预测
 print(f"result is {y_pred}")
